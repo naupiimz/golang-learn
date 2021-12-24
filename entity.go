5da73df48d823b198ecdf7c438ad9945d5cae0c3
naupiimz <naupiimz@gmail.com>
2021-12-24 20:12:55 +0700

    24 dec 2021

---
+++ entity.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func Testing() string {
+	return fmt.Sprintf("Halo nama saya %s dan umur saya %d", "Naupii", 21)
+}
--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"golang-learn/calculation"
+)
 
 func main() {
 	fmt.Println("reset")
+	// sentence := Testing()
+
+	// fmt.Println(sentence)
+
+	result := calculation.Add(12312412, 19484124)
+
+	fmt.Println(result)
 }
