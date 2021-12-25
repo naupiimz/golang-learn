cbd75502e5558e4615f9fe7eec35ceb71175675b
naupiimz <naupiimz@gmail.com>
2021-12-25 18:10:08 +0700

    25 dec 2021

--- main.go
+++ main.go
@@ -3,15 +3,17 @@ package main
 import (
 	"fmt"
 	"golang-learn/calculation"
+	"golang-learn/multiplier"
 )
 
 func main() {
 	fmt.Println("reset")
-	// sentence := Testing()
-
-	// fmt.Println(sentence)
 
 	result := calculation.Add(12312412, 19484124)
 
+	result2 := multiplier.Multiply(12123, 124499)
+
 	fmt.Println(result)
+
+	fmt.Println(result2)
 }
---
+++ multiplier/multiply.go
@@ -0,0 +1,5 @@
+package multiplier
+
+func Multiply(num1 int, num2 int) int {
+	return num1 * num2
+}
