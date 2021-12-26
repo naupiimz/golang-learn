fd74615913758990cd75c739f264e3cc2dadc3fa
naupiimz <naupiimz@gmail.com>
2021-12-26 20:24:44 +0700

    26 dec 2021

--- main.go
+++ main.go
@@ -4,13 +4,30 @@ import "fmt"
 
 func main() {
 
-	title := "golang is the best"
+	// title := "golang is the best"
 
-	for _, val := range title {
-		switch string(val) {
-		case "a", "i", "u", "e", "o":
-			fmt.Println(string(val))
-		}
-	}
+	// for _, val := range title {
+	// 	switch string(val) {
+	// 	case "a", "i", "u", "e", "o":
+	// 		fmt.Println(string(val))
+	// 	}
+	// }
 
+	// var languages [5]string
+
+	// languages[0] = "GO"
+	// languages[1] = "Gin"
+	// languages[2] = "BeeGO"
+	// languages[3] = "Echo"
+	// languages[4] = "Tbd."
+
+	// fmt.Println(languages)
+	// fmt.Println(len(languages))
+
+	var consolesG []string
+
+	consolesG = append(consolesG, "Xbox360")
+	consolesG = append(consolesG, "Switch Pro")
+
+	fmt.Println(consolesG)
 }
