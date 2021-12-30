b4043403bf5c084aaa0c7d2ecdad128c823afcf9
naupiimz <naupiimz@gmail.com>
2021-12-30 17:56:46 +0700

    30 dec 2021

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type User struct {
 	ID       int
 	FName    string
@@ -8,11 +10,31 @@ type User struct {
 	IsActive bool
 }
 
-func main() {
-	user := User{}
-	user.ID = 1980
+type Group struct {
+	Name        string
+	Admin       User
+	Users       []User
+	IsAvailable bool
 }
 
-func displayUser(user string) string {
-	return "vallid"
+func (user User) display() string {
+	return fmt.Sprint(user)
+}
+
+func (group Group) displayGroup() []User {
+
+	return group.Users
+}
+
+func main() {
+	user1 := User{12, "yoyo", "man", "yman", true}
+	user2 := User{11, "ww", "qw", "qq", false}
+
+	userss := []User{user1, user2}
+
+	group := Group{"yoyo", user1, userss, false}
+
+	result := group.displayGroup()
+
+	fmt.Println(result)
 }
